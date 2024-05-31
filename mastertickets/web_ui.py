ecc5dfb5612e31dad7dab155c9098af37aabdbda
Ya55iiine <167767540+Ya55iiine@users.noreply.github.com>
2024-05-31 16:34:42 +0200

    Update web_ui.py

--- mastertickets/web_ui.py
+++ mastertickets/web_ui.py
@@ -312,7 +312,7 @@ class MasterTicketsModule(Component):
             data['graph'] = g
             data['graph_render'] = functools.partial(g.render, self.dot_path)
             data['use_gs'] = self.use_gs
-            data['html_content'] = Markup(graph_render('cmapx'))
+            data['html_content'] = Markup(functools.partial(g.render, 'cmapx'))
 
             #return 'depgraph.html', data, None
             return 'depgraph.html', data, {}
