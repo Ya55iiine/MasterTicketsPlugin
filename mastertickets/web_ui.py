dccae28d5ab18636c00e82c30758df58b414d003
Ya55iiine <167767540+Ya55iiine@users.noreply.github.com>
2024-06-11 10:45:45 +0200

    Update web_ui.py

--- mastertickets/web_ui.py
+++ mastertickets/web_ui.py
@@ -383,7 +383,8 @@ class MasterTicketsModule(Component):
                 #node > g[n]
                 g.edge(str(ticket_id), str(n))
 
-        return g, {}, {} if isinstance(g, tuple) else {'graph': g} 
+        # return g, {}, {} if isinstance(g, tuple) else {'graph': g} 
+        return g
 
     def _link_tickets(self, req, tickets):
         items = []
